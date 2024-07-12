60ec0789be0a5fd390920f8973ad61830161eb7b
jakeNorman007 <jacob.a.norman91@gmail.com>
2024-07-11 23:37:22 -0400

    "create.go

--- cmd/create.go
+++ cmd/create.go
@@ -1,1 +1,27 @@
 package cmd
+
+import (
+	"github.com/JakeNorman007/klarah/cmd/flags"
+	"github.com/JakeNorman007/klarah/cmd/step"
+	"github.com/spf13/cobra"
+)
+
+func Init() {
+    //var flagFramework flags.Framework
+    //var flagDBDriiver flags.Database
+    rootCmd.AddCommand(createCmd)
+}
+
+var createCmd = &cobra.Command {
+	Use:   "create",
+	Short: "",
+	Long: ``,
+
+	Run: func(cmd *cobra.Command, args []string) {
+
+        flagFramework := flags.Framework(cmd.Flag("framework").Value.String())
+		flagDBDriver := flags.Database(cmd.Flag("driver").Value.String())
+
+        steps := step.InitStep(flagFramework, flagDBDriver)
+    },
+}
